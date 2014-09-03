0d5b0180d6250086261bb74551df27fc3fa5799d
hung-phan <colorvisavn@gmail.com>
2014-09-03 19:51:22 +0700

    add helper

--- lib/mithril_rails/rails/railtie.rb
+++ lib/mithril_rails/rails/railtie.rb
@@ -5,13 +5,6 @@ module MithrilRails
       initializer "mithril_rails.add_watchable_files" do |app|
         app.config.watchable_files.concat Dir["#{app.root}/app/assets/javascripts/**/*.msx*"]
       end
-
-      # Include the react-rails view helper lazily
-      initializer "mithril_rails.setup_view_helpers" do
-        ActiveSupport.on_load(:action_view) do
-          include ::Mithril::Rails::ViewHelper
-        end
-      end
     end
   end
 end
